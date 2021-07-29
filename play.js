75b110639c0c814a270ac139f20dcebaa1fd0cc5
Your Name <you@example.com>
2021-07-29 21:30:11 +0000

    ..1st message

--- play.js
+++ play.js
@@ -1,24 +1,25 @@
-const net = require("net");
-
+//const net = require("net");
+const client = require("../client.js");
+const connect = client.connect;
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",// IP address here,
-    port: 50541// PORT number here,
-  });
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "localhost",// IP address here,
+//     port: 50541// PORT number here,
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log('you ded cuz you idled');
-  });
+//   conn.on("connect", () => {
+//     // code that does something when the connection is first established
+//     console.log('you ded cuz you idled');
+//   });
 
 
-  return conn;
+//   return conn;
   
-};
+// };
 
 console.log("Connecting ...");
 connect();
